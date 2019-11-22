7ad0a601b38f1aba97d8be53828f79b6776b9cc9
Qiusheng Wu <giswqs@gmail.com>
2019-11-22 11:11:46 -0500

    Update tensorflow example

--- 14-test-tensorflow.py
+++ 14-test-tensorflow.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python3
 
 import tensorflow as tf
+
+tf.compat.v1.disable_eager_execution()
+
 hello = tf.constant('Hello, TensorFlow!')
-sess = tf.Session()
+
+sess = tf.compat.v1.Session()
+
 print(sess.run(hello))
